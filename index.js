ba997a6cffba46110c5f14a5a7aae5ce4f3627e7
Rabi Kr Yadav <rabikrishnayadav@gmail.com>
2022-02-22 20:10:18 +0545

    validator dependancy add with npm

--- index.js
+++ index.js
@@ -3,13 +3,24 @@ console.log('welcome to learn node');
 const fs = require('fs'); // including file system code module
 
 // Working on Own Module
-const {name, add, sub, mult} = require('./operator.js');
+    // const {name, add, sub, mult} = require('./operator.js');
 
-console.log(name);
-console.log(add(5, 5));
-console.log(sub(8,2));
-console.log(mult(8,2));
+    // console.log('This is ',name);
+    // console.log(add(5, 5));
+    // console.log(sub(8,2));
+    // console.log(mult(8,2));
 
 // for use npm
 // write 'npm init' in command line
 // write the require data at installing time
+
+// use chalk dependency
+    // const chalk = require('chalk');
+    // const log = console.log;
+    // // Combine styled and normal strings
+    // log(chalk.blue('Hello') + ' World' + chalk.red('!'));
+
+const validator = require('validator');
+
+const res = validator.isEmail("rabi@mail.com");
+console.log(res);
