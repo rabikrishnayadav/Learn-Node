30e332c9686effa068a99a5bdb9485e68566c6d2
Rabi Kr Yadav <rabikrishnayadav@gmail.com>
2022-02-22 21:24:16 +0545

    Creating Own web server

--- index.js
+++ index.js
@@ -20,7 +20,45 @@ const fs = require('fs'); // including file system code module
     // // Combine styled and normal strings
     // log(chalk.blue('Hello') + ' World' + chalk.red('!'));
 
-const validator = require('validator');
+// use validator
 
-const res = validator.isEmail("rabi@mail.com");
-console.log(res);
+    // const validator = require('validator');
+
+    // const res = validator.isEmail("rabi@mail.com");
+    // console.log(res);
+
+
+// Web Server
+
+/*
+To access web pages of any web application, you need a web server. The web server will handle all the http requests for the web application
+
+e.g IIS is a web server for ASP.NET web applications and Apache is a web werver for PHP or Java web applications.
+
+Node.js provides capabilities to create own web server which will handle HTTP requests asynchronously.
+We can use IIS or Apache to run Node.js web application but it is recommended to use Node.js web server.
+
+-------------------------------------------
+        Create a Server
+-------------------------------------------
+
+The http.createServer() method includes request and response parameters which is supplied by node.js
+
+The request object can be used to get information about the current HTTP request
+e.g. url, request header, and data.
+
+The response object can be used to send a respose for a current HTTP request.
+
+If the respose from the HTTP server is supposed to be displayed as HTML, we should include an HTTP header with the correct content type
+
+*/
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.end('Hello from the other sides with node');
+});
+
+server.listen(8000,'127.0.0.1', () => {
+    console.log('listening to the port no 8000');
+});
